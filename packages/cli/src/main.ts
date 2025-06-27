b21e8019ed34641b337113b1861a048f75708267
Stefan Haas <stefan.haas.privat@gmail.com>
2025-06-27 16:40:09 +0200

    add commander to cli

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+import { program } from 'commander';
+
+program
+  .name('beyondlint')
+  .description('A rule-engine for triggering actions based on code changes')
+  .version('1.0.0');
+
+program
+  .option('--ci', 'CI Mode (will trigger actions)', 'false')
+  .option('--config <path>', 'Path to the configuration file')
+  .action((options) => {
+    const {
+      config,
+      ci,
+    } = options;
+    console.log(`Configuration file path: ${config}`);
+    console.log(`CI Mode: ${ci}`);
+  });
+
+program.parse(process.argv); 
