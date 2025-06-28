0ee07b22247f509b07e619281409262b4e5241b0
Stefan Haas <stefan.haas.privat@gmail.com>
2025-06-28 23:22:46 +0200

    project beyondlint configs

--- packages/core/src/lib/config/project-config.ts
+++ packages/core/src/lib/config/project-config.ts
@@ -3,5 +3,6 @@ import { RuleConfig } from "./rules-config.js";
 export interface ProjectConfig {
   extends: string;
   tsconfigPath: string;
+  sourceRoot: string;
   rules?: Record<string, RuleConfig>;
 }
